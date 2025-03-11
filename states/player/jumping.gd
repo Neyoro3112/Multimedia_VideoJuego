601be33539210130439f8485fa87f50a852a92e5
CarlosDRomero <cdavid.romeror@gmail.com>
2025-03-11 14:24:40 -0500

    add jump cut when the jump action is released

--- states/player/jumping.gd
+++ states/player/jumping.gd
@@ -18,6 +18,8 @@ func physics_update(delta: float):
 	# Mientras el botón de salto esté presionado, permite extender el salto
 	if Input.is_action_pressed("jump") and not timer.is_stopped():
 		player.velocity.y = -player.jump_speed
+	elif not Input.is_action_pressed("jump"):
+		player.velocity.y = 0		
 	else:
 		timer.stop()
 
