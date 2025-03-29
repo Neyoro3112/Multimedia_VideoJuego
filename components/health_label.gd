e755a773b6f3eca06bc45bb6f68191347d0f3571
CarlosDRomero <cdavid.romeror@gmail.com>
2025-03-28 22:01:49 -0500

    allow dissolve effect for variable spritesheets

--- components/health_label.gd
+++ components/health_label.gd
@@ -1,5 +1,8 @@
 extends Label
 
+@onready var health: HealthComponent = $"../Health"
+func _ready():
+	text = str(health.health)
 
 func _on_health_health_changed(current: int,_diff: int) -> void:
 	text = str(current)
