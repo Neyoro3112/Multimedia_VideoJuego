d7e0aecfb4a7f7422a8e689cc5d0526cd10dc76d
CarlosDRomero <cdavid.romeror@gmail.com>
2025-03-31 15:35:17 -0500

    fix some bugs on the ignythos pickup mechanic

--- characters/Ignythos/ignythos.gd
+++ characters/Ignythos/ignythos.gd
@@ -4,28 +4,34 @@ var follow_marker: Marker2D = null
 @export var following_entity: Entity :
 	set(entity):
 		following_entity = entity
+		pickup_area.set_deferred("monitoring", following_entity == null)
+		print("Setting monitoring: ", following_entity)
 		
 
 @onready var point_light_2d: PointLight2D = $PointLight2D
 @onready var sprite_2d: Sprite2D = $Node2D/Sprite2D
 @onready var pickup_area: Area2D = $PickupArea
 
-#func _ready():
-	#
+func _ready():
+	if not following_entity: following_entity = null
 
 func get_follow_marker_from_entity() -> Marker2D:
-	if following_entity and following_entity is Player:
-		var markers = following_entity.get_tree().get_nodes_in_group("follow_marker")
-		if markers.size() > 0:
-			return markers[0]
+	if not following_entity: 
+		return null
+	for marker in get_tree().get_nodes_in_group("follow_marker"):
+		if marker.owner == following_entity:
+			return marker
 	return null
+	
 var dead: bool
 func _physics_process(_delta: float):
 	if not follow_marker and following_entity:
 		follow_marker = get_follow_marker_from_entity()
+		if not follow_marker:
+			sprite_2d.visible = false
 	if dead:
 		point_light_2d.energy = lerp(point_light_2d.energy, 0.4, 0.5*_delta)
-		velocity.y = 1000 * _delta
+		velocity.y = gravity * _delta
 		move_and_slide()
 		if is_on_floor():
 			animation_controller.update_animation("blink")
@@ -35,18 +41,16 @@ func _physics_process(_delta: float):
 	
 	if not following_entity.hurtbox.healthComponent.is_alive:
 		following_entity = null
-		
+		follow_marker = null
 		point_light_2d.energy = 10
-		velocity.y = -1000 * _delta
+		velocity.y = gravity * _delta
 		move_and_slide()
-		pickup_area.monitoring = true
 		animation_controller.update_animation("fall")
 		sprite_2d.visible = true
 		dead = true
 	elif follow_marker:
 		global_position = global_position.slerp(follow_marker.global_position, 0.1)
 	else:
-		print("Entity position")
 		global_position = following_entity.global_position
 
 
@@ -54,7 +58,4 @@ func _on_pickup_area_body_entered(body: Entity) -> void:
 	if following_entity or body is not Entity: return
 	dead = false
 	following_entity = body
-	sprite_2d.visible = false
-	pickup_area.monitoring = false
-	follow_marker = null
-	print("Entity: ", following_entity)
+	
--- states/entities/player/death.gd
+++ states/entities/player/death.gd
@@ -4,12 +4,14 @@ class_name PlayerDeath
 @export var knockback_movement_component: KnockbackMovement
 @export var particles_scene: PackedScene
 @export var particles_position_marker: Marker2D
+@onready var world_collider: CollisionShape2D = $"../../WorldCollider"
 
 func enter():
 	player.movement_component = knockback_movement_component
 	knockback_movement_component.init(player)
 	player.animation_controller.update_animation(PlayerAnimations.Death)
 	player.movement_fsm.paused = true
+	player.set_collision_layer_value(7, false)
 
 func physics_update(delta: float):
 	player.update_physics(delta)
